17270f4414cb354e010e765ad8733b0e0e71d0cb
Georgi Georgiev <gogolino1982@gmail.com>
2023-09-24 11:44:35 +0300

    css setup

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const path = require('path');
 
 function expressConfigurator(app) {
-    app.use(express.static('../content'));
+    app.use(express.static(path.resolve(__dirname, '../content')));
     app.use(express.urlencoded({ extended: false }));
 }
 
